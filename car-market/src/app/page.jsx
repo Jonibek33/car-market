fc3ecb8da14ab6a0e7d9009c2b847f94e9c78326
Jonibek33 <xalilov3148635@gmail.com>
2025-09-20 06:17:14 +0500

    part 1 (card 1)

--- car-market/src/app/page.jsx
+++ car-market/src/app/page.jsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import Link from "next/link";
+import Image from "next/image";
+import { ChartNoAxesColumnDecreasing, Heart } from 'lucide-react';
+
+function Home() {
   return (
-    <div>
-      
-    </div>
+    
+    <main>
+      <div className="part_1">
+        <div className="conteiner">
+          <h1>Автомобили в наличии с ПТС</h1>
+          <div className="cards">
+            <div className="card">
+              <h2>Skoda Octavia <br/> 1.6 MPI MT Active </h2>
+              <div className="icons">
+                <Heart color="#737373" />
+                <ChartNoAxesColumnDecreasing color="#737373" />
+              </div>
+              <Image 
+                src="/predlozhenie_dnya.png" 
+                alt="Предложение дня" 
+                height={20} 
+                width={120}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
   );
 }
+export default Home;
